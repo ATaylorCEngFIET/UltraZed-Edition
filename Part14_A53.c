dffa2e64d93b9e4a1dc956dd69ef71e2528fc3b3
Adam Taylor <aptaylor@theiet.org>
2017-06-04 14:45:39 +0100

    Part14
    Addressing PL elements from the PS

---
+++ Part14_A53.c
@@ -0,0 +1,64 @@
+/*Copyright (c) 2017, Adam Taylor
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project*/
+
+#include <stdio.h>
+#include "platform.h"
+#include "xbram.h"
+
+#define BRAM_DEVICE_ID		XPAR_BRAM_0_DEVICE_ID
+
+XBram Bram;
+
+int main()
+{
+    init_platform();
+
+    printf("UltraZed Edition Part 14\n\r");
+    printf("http://www.ultrazedchronicles.com\n\r");
+	int  i;
+	u32 data;
+
+	XBram_Config *ConfigPtr;
+
+
+	ConfigPtr = XBram_LookupConfig(BRAM_DEVICE_ID);
+	XBram_CfgInitialize(&Bram, ConfigPtr,ConfigPtr->CtrlBaseAddress);
+
+	for (i=0; i<256 ;i++){
+		data = XBram_ReadReg(ConfigPtr->MemBaseAddress, (i*4));
+		printf("Data Read from Address %x %x \n\r",(unsigned int)(i*4),(unsigned int) data );
+	}
+
+	for (i=0; i<256 ;i++){
+		XBram_WriteReg(ConfigPtr->MemBaseAddress, (i*4), i);
+		printf("Data Written to Address %x %x \n\r",(unsigned int)(i*4),(unsigned int)i );
+	}
+
+	for (i=0; i<256 ;i++){
+		data = XBram_ReadReg(ConfigPtr->MemBaseAddress, (i*4));
+		printf("Data Read from Address %x %x \n\r",(unsigned int)(i*4),(unsigned int)data );
+	}
+
+    cleanup_platform();
+    return 0;
+}
